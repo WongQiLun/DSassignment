a778dcd5d2879fc390df355cff2d9ba458f15abd
jameswong98 <jameswong-98_@hotmail.com>
2019-12-22 01:07:48 +0800

    duplicate word v2

---
+++ src/Class/DuplicateWord.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+
+
+/**
+ *
+ * @author Forge-15 1650
+ */
+public abstract class DuplicateWord implements DuplicateWordADT{
+
+    
+    public String sContains(String word, int para) {
+        
+    }
+
+  
+    public String sEnd(String word, int para) {
+  
+    }
+
+  
+    //public int sInt(String word, int para) {
+  
+    }
+
+    
+    public String sStart(String word, int para) {
+        
+    }
+
+    
+    public String sWhole(String word, int para) {
+        
+    }
+    
+}
