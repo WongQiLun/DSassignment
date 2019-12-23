c09d6e242970744e9331ee3196d99615f64fb16a
WongQi <wongql-wp16@student.tarc.edu.my>
2019-12-23 11:06:16 +0800

    added time needed display on adt

--- src/Class/WordDoublyLinkedList.java
+++ src/Class/WordDoublyLinkedList.java
@@ -252,11 +252,15 @@ public class WordDoublyLinkedList<T extends Comparable<? super T>>  implements D
     // The main function to sort a linked list. It mainly calls _quickSort() 
     public void quickSort() 
     { 
+        long startTime = System.currentTimeMillis();
         // Find last node 
         Node head = getLastNode(); 
         Node node = getFirstNode();
         // Call the recursive QuickSort 
         _quickSort(node,head); 
+        long endTime=System.currentTimeMillis();
+        long timeNeeded = endTime - startTime;
+          System.out.println(" SORTING TIME "+ timeNeeded+"ns");
     } 
 
 }
